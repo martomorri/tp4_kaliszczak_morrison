e4b78dee68b3ced0a7a01a76cf81c8faa2b5d458
martomorri <106319796+martomorri@users.noreply.github.com>
2023-05-17 09:05:45 -0300

    js es el peor invento de la sociedad

--- src/App/App.js
+++ src/App/App.js
@@ -3,27 +3,69 @@ import './App.css';
 import Subtitle from './Subtitle/Subtitle';
 import Input from './Input/Input';
 import Cita from './Cita/Cita';
+import { useState } from 'react';
 
 function App() {
+  const [citas, setCitas] = useState([
+    {
+      "mascota":"Nina",
+      "propietario":"Martin",
+      "fecha":"2021-08-05",
+      "hora":"08:20",
+      "sintomas":"Le duele la pierna"
+    },
+    {
+      "mascota":"Sifon",
+      "propietario":"Flecha",
+      "fecha":"2023-08-05",
+      "hora":"09:24",
+      "sintomas":"Duerme mucho"
+    },
+    {
+      "mascota":"Floki",
+      "propietario":"Ari",
+      "fecha":"2023-08-05",
+      "hora":"16:15",
+      "sintomas":"No está comiendo"
+    }
+  ]);
+  const agregarCita = e => {
+    e.preventDefault();
+    setCitas(
+      [
+        ...citas,
+        e.target.mascota.value,
+        e.target.propietario.value,
+        e.target.fecha.value,
+        e.target.hora.value,
+        e.target.sintomas.value
+      ]
+    );
+    e.target.value = "";
+  }
+  
   return (
     <div className="App">
       <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <div class="container">
-        <div class="row">
-          <div class="one-half column">
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
             <Subtitle texto="Crear mi Cita" />
-            <Input label="Nombre Mascota" type="text" name="mascota" placeholder="Nombre Mascota" />
-            <Input label="Nombre Dueño" type="text" name="propietario" placeholder="Nombre dueño de la mascota" />
-            <Input label="Fecha" type="date" name="fecha" />
-            <Input label="hora" type="time" name="hora" />
-            <Input label="Sintomas" type="textarea" name="sintomas" />
-            <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
+            <form onSubmit={(e) => agregarCita(e)}>
+              <Input label="Nombre Mascota" type="text" name="mascota" placeholder="Nombre Mascota" />
+              <Input label="Nombre Dueño" type="text" name="propietario" placeholder="Nombre dueño de la mascota" />
+              <Input label="Fecha" type="date" name="fecha" />
+              <Input label="hora" type="time" name="hora" />
+              <Input label="Sintomas" type="textarea" name="sintomas" />
+              <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
+            </form>
           </div>
-          <div class="one-half column">
+          <div className="one-half column">
             <Subtitle texto="Administra tus citas" />
-            <Cita nombre="Nina" dueño="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna" />
+            {citas.map(cita => <Cita nombre={cita.mascota} dueño={cita.propietario} fecha={cita.fecha} hora={cita.hora} sintomas={cita.sintomas} />)}
+            {/* <Cita nombre="Nina" dueño="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna" />
             <Cita nombre="Sifon" dueño="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho" />
-            <Cita nombre="Floki" dueño="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo" />
+            <Cita nombre="Floki" dueño="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo" /> */}
           </div>
         </div>
       </div>
--- src/App/Input/Input.js
+++ src/App/Input/Input.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Input.css';
 
 export default function Input(props = "") {
-    let input = null;
+    let input;
     if (props.type !== "textarea") {
-        input = <input type={props.type} name={props.name} className="u-full-width" placeholder={props.placeholder} value="" />
+        input = <input type={props.type} name={props.name} className="u-full-width" placeholder={props.placeholder} value=""></input>
     }
     else input = <textarea name={props.name} className="u-full-width"></textarea>
     return (
